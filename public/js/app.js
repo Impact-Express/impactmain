47b256c23a9a65cb0d164a96efa844597a920fff
Cameron Miller <cambomiller@googlemail.com>
2019-12-16 17:07:16 +0000

    Start Work on new Navbar and make sure it looks good on mobile

--- public/js/app.js
+++ public/js/app.js
@@ -19314,8 +19314,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/impactmain/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/impactmain/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/impact-main/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/impact-main/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,19 +2,24 @@
 
 @section('master')
 
-    <div class="top-strip"></div>
+    <div class="top-strip">
+        <img class="top-strip-img" src="img/dhl-strip-img.png" alt="DHL Authorised Service Partner">
+    </div>
     <nav class="main-nav">
-
-        <a class="navbar-logo" href="#"><img class="nav-logo-img" src="img/navbar-logo.png" alt="Impact Express Logo"></a>
+        <a class="navbar-logo" href="#">
+            <img class="nav-logo-img" src="img/navbar-logo.png" alt="Impact Express Logo">
+        </a>
         <div class="hamburger-container">
             <button class="hamburger button-square" type="button">
                 <span class="hamburger-icon"><i class="fas fa-bars"></i></span>
             </button>
         </div>
         <ul class="nav-items">
-            <li class="nav-item"><a class="nav-link" href="#">Navigation Tab 1</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Navigation Tab 2</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Navigation Tab 3</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Track My Parcel</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Shipping</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">News & Events</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
+            <li class="nav-item"><a class="nav-link button-main" href="#">Get a Quote</a></li>
             <li class="nav-item"><a class="nav-link button-main" href="#">Log in</a></li>
         </ul>
     </nav>
